c846822a8e35801a3d30d27d66d7f8c7c52c7101
DenCoder618 <denis100703@gmail.com>
2021-02-27 18:39:02 +0300

    Test for login added

--- login_test.py
+++ login_test.py
@@ -32,53 +32,34 @@ comments_link = "https://w41k3r.com/comments/feed/"
 login_link = "https://w41k3r.com/wp-login.php"
 alan_link = "https://w41k3r.com/walkers/?w_id=0"
 
-def parse_rss(link):
-    global old_hashes
-    feed = feedparser.parse(link)
-    hashes = []
-    for entry in feed.entries:
-        hash = h.md5(entry.link.encode('utf-8')).hexdigest()
-        hashes.append(hash)
-        if not hash in old_hashes:
-            p_link = entry['links'][0]['href']
+def send_file(code):
+    text_file = open("parse.txt", "w")
+    text_file.write(code)
+    text_file.close()
+    text_file = open("parse.txt", "r")
+    msg = bot.send_document(channel, text_file)
+    text_file.close()
+
+def parse_rss(p_link):
             c = requests.session()
             login_data = {"log": login,
                           "pwd": password,
                           "rememberme": "forever",
                           "redirect_to": p_link,
                           "redirect_to_automatic": "1"}
-            page_login = c.post(login_link, data=login_data)
-            # получаем данные
-            p = c.get(p_link, headers=random_headers())
+
+            head = random_headers()
+            page_login = c.post(login_link, data=login_data, headers=head)
+            p = c.get(p_link, headers=head)
             temp = bs.BeautifulSoup(p.content, features="html.parser")
-            text_file = open("parse.txt", "w")
-            text_file.write(str(temp.find("main").encode("utf-8")))
-            text_file.close()
-            text_file = open("parse.txt", "r")
-            msg = bot.send_document(channel, text_file)
-            text_file.close()
+            send_file(str(temp.find("main").encode("utf-8")))
+
             #print(temp)
             #print(temp.find("main"))
-
-            #s = bs.BeautifulSoup(p.content, features="html.parser").find("main")
-            #title = s.find("h1").text
-            #wid = s.find("span", {'class': 'wid'}).text
-            #t = s.find("article").text
-#
-            #while "\n\n" in t: t = t.replace("\n\n", "\n")
-            #if t.startswith("\n"): t = t[1:]
-            #if t.endswith("\n"): t = t[:-1]
-            #mess = "Post by #" + wid + "\n*" + title + "*\n\n" + t + "\n\n" + p_link
-            #send_message(mess)
-
-    old_hashes = hashes
 
 def send_message(message):
     msg = bot.send_message(channel, message, parse_mode='Markdown', disable_web_page_preview=True)
 
 if __name__ == '__main__':
     bot = telebot.TeleBot(token)
-    old_hashes = [h.md5("lol".encode('utf-8')).hexdigest()]
-    while True:
-        parse_rss(rss_link)
-        sleep(2)
+    parse_rss("https://w41k3r.com/i-know-why-they-are-riding-horses/")
